a6f5f0c642e334ae4a9fa5c4bcd850b84188bd2e
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2025-03-21 13:18:51 +0100

    Favorites: localstorage update to watchlist slice

--- src/store/watchlistSlice.js
+++ src/store/watchlistSlice.js
@@ -1,7 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const loadFavoritesFromLocalStorage = () => {
+    try {
+        const serializedState = localStorage.getItem('favorites');
+        if (serializedState === null) {
+            return [];
+        }
+        return JSON.parse(serializedState);
+    } catch (e) {
+        console.error('Could not load favorites from local storage');
+        return [];
+    }
+};
+
+const saveFavoritesToLocalStorage = (favorites) => {
+    try {
+        const serializedState = JSON.stringify(favorites);
+        localStorage.setItem('favorites', serializedState);
+    } catch (e) {
+        console.error('Could not save favorites from local storaged');
+    }
+};
+
 const initialState = {
-    favorites: [],
+    favorites: loadFavoritesFromLocalStorage(),
 };
 
 const watchlistSlice = createSlice({
@@ -10,11 +32,13 @@ const watchlistSlice = createSlice({
     reducers: {
         addToFavorites: (state, action) => {
             state.favorites.push(action.payload);
+            saveFavoritesToLocalStorage(state.favorites);
         },
         removeFromFavorites: (state, action) => {
             state.favorites = state.favorites.filter(
                 (item) => item.id !== action.payload.id
             );
+            saveFavoritesToLocalStorage(state.favorites);
         },
     },
 });
